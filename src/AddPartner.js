f1683a2ff16f814c090ae4c88b4580826d339ec4
minnie <maninderjeet28@gmail.com>
2023-06-20 18:40:55 +0530

    new changes

---
+++ src/AddPartner.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import NamePartners from './NamePartners';
+
+function AddPartnerForm() {
+  const [showPartners, setShowPartners] = useState(false);
+  const [partnerData, setPartnerData] = useState({
+    id: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+  const [partners, setPartners] = useState([]);
+
+  const handleChange = (e) => {
+    setPartnerData({ ...partnerData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (partnerData.id && partnerData.firstName && partnerData.lastName && partnerData.email) {
+      addPartner(partnerData);
+
+      setPartnerData({
+        id: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+      });
+
+      fetch('http://localhost:8080/api/partners')
+      .then((response) => response.json())
+      .then((data) => {
+      
+        console.log('Updated data:', data);
+   
+        setPartners(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+
+    } else {
+      console.log('Please fill in all fields');
+    }
+  };
+
+  const addPartner = (partner) => {
+    console.log('Adding partner:', partner);
+    setPartners([...partners, partner]); 
+    setShowPartners(true); 
+  };
+
+  return (
+    <div>
+      <h2>Add Partner</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="id">ID:</label>
+          <input
+            type="text"
+            id="id"
+            name="id"
+            value={partnerData.id}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            value={partnerData.firstName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            value={partnerData.lastName}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={partnerData.email}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {showPartners && <NamePartners partners={partners} />} 
+    </div>
+  );
+}
+
+export default AddPartnerForm;
--- src/App.js
+++ src/App.js
@@ -1,40 +1,69 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NamePartners from './NamePartners';
 import Associates from './Associates';
-import Cases from './Cases ';
+import AddPartner from './AddPartner';
+// import Cases from './Cases';
 
 function App() {
-
   const [data, setData] = useState([]);
+  const [showPartners, setShowPartners] = useState(false);
+  const [partners, setPartners] = useState([]);
+  const [submittedData, setSubmittedData] = useState(null);
 
-   
-    useEffect(() => {
-        fetch('http://localhost:8080/', {mode: 'cors'})
-          .then((res) => res.json())
-          .then((data) => {
-            setData(data);
-          })
-          .catch((err) => {
-            console.log(err.message);
-          });
-   }, []);
+  const addPartner = (partner) => {
+    setPartners([...partners, partner]);
+  };
 
-      return (
-        <div className="App">
-          <h2> HELLO WORLD</h2>   
-          <h1>WELCOME WORLD!!</h1>
-          <NamePartners />
-          <Associates />
-          <Cases />
+  const submitData = () => {
+    const newData = {};
+    setSubmittedData(newData);
+  };
 
-          <ul>
-      {!data ? 'loading...' : 
-        <li key={data.data}>{data.data}</li>
-      }
-    </ul>
-   
+  const handleHomeButtonClick = () => {
+    setShowPartners(true);
+  };
+
+  const handleAddPartnerClick = () => {
+    setShowPartners(false);
+  };
+
+  useEffect(() => {
+    fetch('http://localhost:8080/', { mode: 'cors' })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <div>
+        <button onClick={handleHomeButtonClick}>Home</button>
+        {showPartners && <NamePartners />}
+        
+        <button onClick={handleAddPartnerClick}>Add Partner</button>
+        {showPartners || <AddPartner onSubmit={submitData} />}
+      </div>
+
+      <h2>HELLO WORLD</h2>
+      <h1>WELCOME WORLD!!</h1>
+
+      {/* <Associates />
+      <Cases /> */}
+
+      <ul>
+        {!data ? (
+          'loading...'
+        ) : (
+          <li key={data.data}>{data.data}</li>
+        )}
+      </ul>
+      
     </div>
-      );
+  );
 }
 
 export default App;
